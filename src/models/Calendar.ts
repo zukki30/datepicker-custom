61f8bb74f1bcf7039ea8a2c9545f1cc77eca7249
kk-watanabe <kazuki.watanabe@bebit.com>
2020-02-27 18:08:57 +0900

    feat: Add getter method
    - showMonth
    - title
    - prevCalendar
    - nextCalendar

--- src/models/Calendar.ts
+++ src/models/Calendar.ts
@@ -1,12 +1,82 @@
 export const weeks = ["sun", "mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const MAX_MONTH = 12;
 
 export class Calendar {
   constructor(
     // 対象日
     public readonly date: Date,
     // 表示する前の月
-    public readonly monthsAgo: number = 3,
+    public readonly monthsAgo: number = 2,
     // 週の始まり
     public readonly weekStart: number = 0
   ) {}
+
+  get showMonth(): number {
+    return this.date.getMonth() + 1;
+  }
+
+  get title(): string {
+    return this.date.getFullYear() + "年" + this.showMonth + "月";
+  }
+
+  get prevCalendar(): PrevCalendar {
+    const prevMonthsAgo = this.monthsAgo + 1;
+    return PrevCalendar.build(
+      this.date.getFullYear(),
+      this.date.getMonth() - prevMonthsAgo,
+      this.showMonth - prevMonthsAgo
+    );
+  }
+
+  get nextCalendar(): NextCalendar {
+    return NextCalendar.build(
+      this.date.getFullYear(),
+      this.date.getMonth() + 1,
+      this.showMonth + 1
+    );
+  }
+}
+
+export class PrevCalendar {
+  constructor(
+    public readonly year: number,
+    public readonly month: number,
+    public readonly showMonth: number
+  ) {}
+
+  public static build(
+    year: number,
+    month: number,
+    showMonth: number
+  ): PrevCalendar {
+    if (0 > month) {
+      return new PrevCalendar(
+        year - 1,
+        MAX_MONTH + month,
+        MAX_MONTH + showMonth
+      );
+    }
+
+    return new PrevCalendar(year, month, showMonth);
+  }
+}
+
+export class NextCalendar {
+  constructor(
+    public readonly year: number,
+    public readonly month: number,
+    public readonly showMonth: number
+  ) {}
+
+  public static build(
+    year: number,
+    month: number,
+    showMonth: number
+  ): NextCalendar {
+    if (month >= MAX_MONTH) {
+      return new NextCalendar(year + 1, 0, 1);
+    }
+
+    return new NextCalendar(year, month, showMonth);
+  }
 }
