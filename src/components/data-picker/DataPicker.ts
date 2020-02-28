d2090df5a2a8f2abd02ca9030dc466c51fbf5b63
kk-watanabe <kazuki.watanabe@bebit.com>
2020-02-28 12:09:46 +0900

    feat: Add DataPicker models

---
+++ src/components/data-picker/DataPicker.ts
@@ -0,0 +1,33 @@
+import {
+  Calendar,
+  PrevCalendar,
+  NextCalendar
+} from "@/components/calendar/Calendar";
+
+export class DataPicker {
+  constructor(
+    // 対象日
+    public readonly date: Date,
+    // 表示する前の月
+    public readonly monthsAgo: number = 2
+  ) {}
+
+  get prevCalendar(): PrevCalendar {
+    const showMonth = this.date.getMonth() + 1;
+    const prevMonthsAgo = this.monthsAgo + 1;
+    return PrevCalendar.build(
+      this.date.getFullYear(),
+      this.date.getMonth() - prevMonthsAgo,
+      showMonth - prevMonthsAgo
+    );
+  }
+
+  get nextCalendar(): NextCalendar {
+    const showMonth = this.date.getMonth() + 1;
+    return NextCalendar.build(
+      this.date.getFullYear(),
+      this.date.getMonth() + 1,
+      showMonth + 1
+    );
+  }
+}
