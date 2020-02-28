84a83bd299017da960b59b64faa578e842ed11f0
kk-watanabe <kazuki.watanabe@bebit.com>
2020-02-28 18:30:09 +0900

    feat: Add CalnderTable component

---
+++ src/components/calendar/CalendarTable.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="calendar-table">
+    <div class="calendar-table__title">{{ title }}</div>
+    <table class="calendar-table__body">
+      <thead>
+        <tr>
+          <th
+            v-for="(week, index) in weeks"
+            :key="index"
+            class="calendar-table__cell"
+          >
+            {{ week }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, index) in tableDatas" :key="index">
+          <td
+            v-for="date in row"
+            :key="date.getDate()"
+            class="calendar-table__cell"
+            @mouseenter="onMouseEnter"
+            @mouseleave="onMouseLeave"
+            @click="onClick"
+          >
+            {{ date.getDate() }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Model, Emit } from "vue-property-decorator";
+import { Calendar, Table } from "@/components/calendar/Calendar";
+
+@Component
+export default class CalendarTable extends Vue {
+  @Prop({ type: Calendar, required: true })
+  calendar!: Calendar;
+
+  @Emit("mouse-enter")
+  onMouseEnter(date: Date) {}
+
+  @Emit("mouse-leave")
+  onMouseLeave(date: Date) {}
+
+  @Emit("click")
+  onClick(date: Date) {}
+
+  get title(): string {
+    return this.calendar.title;
+  }
+
+  get weeks(): string[] {
+    return this.calendar.weekTexts;
+  }
+
+  get tableDatas(): Table {
+    return this.splitTtableDatas(
+      this.calendar.createTabeDates,
+      this.weeks.length
+    );
+  }
+
+  // 配列を指定の数で分割する
+  splitTtableDatas(dates: Date[], splitNumber: number): Table {
+    return dates.reduce(
+      (acc, value, i) =>
+        i % splitNumber ? acc : [...acc, dates.slice(i, i + splitNumber)],
+      [] as Table
+    );
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.calendar-table {
+  &__title {
+    padding: 10px;
+    border-bottom: 1px solid $colorBase500;
+    text-align: center;
+    font-weight: bold;
+    font-size: 16px;
+  }
+
+  &__body {
+    width: 100%;
+    table-layout: fixed;
+  }
+
+  &__cell {
+    padding: 10px;
+    vertical-align: middle;
+    text-align: center;
+    font-size: 12px;
+  }
+}
+</style>
