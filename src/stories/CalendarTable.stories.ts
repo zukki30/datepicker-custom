3b66fcbd0793c1f0d2132a570a390a10ce3a5e2a
kk-watanabe <kazuki.watanabe@bebit.com>
2020-03-02 20:31:08 +0900

    feat: Add range mode story

--- src/stories/CalendarTable.stories.ts
+++ src/stories/CalendarTable.stories.ts
@@ -1,42 +1,90 @@
 import { storiesOf } from "@storybook/vue";
 import { date } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
-import { Calendar } from "@/components/calendar/Calendar";
+import { Calendar, DateRange } from "@/components/calendar/Calendar";
 import CalendarTable from "@/components/calendar/CalendarTable.vue";
 
-storiesOf("CalendarTable", module).add(
-  "default",
-  () => ({
-    components: { CalendarTable },
-    props: {
-      date: {
-        default: myDateKnob("Date", new Date())
-      }
-    },
-    methods: {
-      onMouseEnter: action("mouse-enter"),
-      onMouseLeave: action("mouse-leave"),
-      onClick: action("click")
-    },
-    computed: {
-      calendar() {
-        const props: any = this.$props;
-        const date: Date = props.date;
-        return Calendar.build(date);
-      }
-    },
-    template: `<CalendarTable
+const dateRange: DateRange = {
+  min: new Date("2020-03-15"),
+  max: new Date("2020-03-25")
+};
+
+storiesOf("CalendarTable", module)
+  .add(
+    "default",
+    () => ({
+      components: { CalendarTable },
+      props: {
+        date: {
+          default: myDateKnob("Calendar", new Date())
+        },
+        dates: {
+          default: myDateKnob("Dates", new Date("2020-03-15"))
+        }
+      },
+      methods: {
+        onMouseEnter: action("mouse-enter"),
+        onMouseLeave: action("mouse-leave"),
+        onClick: action("click")
+      },
+      computed: {
+        calendar() {
+          const props: any = this.$props;
+          const date: Date = props.date;
+          return Calendar.build(date);
+        }
+      },
+      template: `<CalendarTable
      :calendar="calendar"
+     :dates="dates"
      @click="onClick"
      @mouse-enter="onMouseEnter"
      @mouse-leave="onMouseLeave" />`
-  }),
-  {
-    info: {
-      summary: "Simple CalendarTable component"
+    }),
+    {
+      info: {
+        summary: "Simple CalendarTable component"
+      }
     }
-  }
-);
+  )
+  .add(
+    "range mode",
+    () => ({
+      components: { CalendarTable },
+      data() {
+        return { dates: dateRange };
+      },
+      props: {
+        date: {
+          default: myDateKnob("Calendar", new Date())
+        }
+      },
+      methods: {
+        onMouseEnter: action("mouse-enter"),
+        onMouseLeave: action("mouse-leave"),
+        onClick: action("click")
+      },
+      computed: {
+        calendar() {
+          const props: any = this.$props;
+          const date: Date = props.date;
+          return Calendar.build(date);
+        }
+      },
+      template: `<CalendarTable
+     :calendar="calendar"
+     :dates="dates"
+     :range-mode="true"
+     @click="onClick"
+     @mouse-enter="onMouseEnter"
+     @mouse-leave="onMouseLeave" />`
+    }),
+    {
+      info: {
+        summary: "Simple CalendarTable component"
+      }
+    }
+  );
 
 function myDateKnob(name: string, value: Date) {
   const stringTimestamp = date(name, value);
