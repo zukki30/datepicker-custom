2db672db3049fe33e801f190f3974aad99dfaed1
kk-watanabe <kazuki.watanabe@bebit.com>
2020-03-03 13:19:37 +0900

    feat: Add components and story to CalendarDateRangeTable

---
+++ src/components/calendar/CalendarDateRangeTable.vue
@@ -0,0 +1,199 @@
+<template>
+  <CalendarBase
+    v-slot:default="{ date }"
+    class="calendar-date-table"
+    :calendar="calendar"
+    @click="onDateClick"
+  >
+    <div class="calendar-date-range-table__inner" :class="addRangeClass(date)">
+      <div class="calendar-date-range-table__date" :class="addDateClass(date)">
+        {{ date.getDate() }}
+      </div>
+    </div>
+  </CalendarBase>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Emit } from "vue-property-decorator";
+import { Calendar, DateRange } from "@/components/calendar/Calendar";
+import CalendarBase from "@/components/calendar/CalendarBase.vue";
+
+@Component({
+  components: { CalendarBase }
+})
+export default class CalendarDateRangeTable extends Vue {
+  @Prop({ type: Calendar, required: true })
+  calendar!: Calendar;
+
+  @Prop({ type: Object, default: null })
+  selectedDates!: DateRange;
+
+  @Emit("click")
+  onClick(dates: DateRange) {}
+
+  selectDates: Date[] =
+    this.selectedDates !== null
+      ? [this.selectedDates.min, this.selectedDates.max]
+      : [];
+
+  onDateClick(date: Date) {
+    if (this.selectDates.length === 2) {
+      console.log(this.selectDates);
+      this.selectDates = [];
+    }
+
+    this.selectDates.push(date);
+
+    if (this.selectDates.length === 2) {
+      const dateRange = this.changeDateRange(this.selectDates);
+      this.onClick(dateRange);
+    }
+  }
+
+  addDateClass(date: Date): string[] {
+    const addClass: string[] = [];
+
+    // 今日の日付に付与
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    if (date.getTime() === today.getTime()) {
+      addClass.push("calendar-date-range-table__date--today");
+    }
+
+    // prop dates の日付に付与
+    if (this.isSelectedDate(date)) {
+      addClass.push("calendar-date-range-table__date--selected");
+    }
+
+    return addClass;
+  }
+
+  isSelectedDate(date: Date): boolean {
+    if (this.selectDates.length > 0) {
+      const selectedMinDate = this.selectDates[0];
+      selectedMinDate.setHours(0, 0, 0, 0);
+
+      const selectedMaxDate =
+        this.selectDates.length === 2
+          ? this.selectDates[1]
+          : this.selectDates[0];
+      selectedMaxDate.setHours(0, 0, 0, 0);
+
+      const dateRange = this.changeDateRange([
+        selectedMinDate,
+        selectedMaxDate
+      ]);
+
+      // 選択済みの日付に付与
+      return (
+        dateRange.min.getTime() === date.getTime() ||
+        dateRange.max.getTime() === date.getTime()
+      );
+    }
+
+    return false;
+  }
+
+  addRangeClass(date: Date): string {
+    const selectedMinDate = this.selectDates[0];
+    selectedMinDate.setHours(0, 0, 0, 0);
+
+    const selectedMaxDate =
+      this.selectDates.length === 2 ? this.selectDates[1] : this.selectDates[0];
+    selectedMaxDate.setHours(0, 0, 0, 0);
+
+    const dateRange = this.changeDateRange([selectedMinDate, selectedMaxDate]);
+
+    if (dateRange.min.getTime() === date.getTime()) {
+      return "calendar-date-range-table__inner--range-start";
+    }
+
+    if (dateRange.max.getTime() === date.getTime()) {
+      return "calendar-date-range-table__inner--range-end";
+    }
+
+    if (
+      dateRange.max.getTime() > date.getTime() &&
+      dateRange.min.getTime() < date.getTime()
+    ) {
+      return "calendar-date-range-table__inner--range";
+    }
+
+    return "";
+  }
+
+  changeDateRange(dates: Date[]): DateRange {
+    const dateRange: DateRange = {
+      min: dates[1] > dates[0] ? dates[0] : dates[1],
+      max: dates[1] > dates[0] ? dates[1] : dates[0]
+    };
+    return dateRange;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+$cellHeight: 35px;
+
+.calendar-date-range-table {
+  &__inner {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: $cellHeight;
+    &:before {
+      position: absolute;
+      top: 50%;
+      z-index: 0;
+      margin-top: -12px;
+      height: 24px;
+      background-color: #f2f5fc;
+      content: "";
+    }
+
+    &--range {
+      &:before {
+        left: 0;
+        width: 100%;
+      }
+    }
+
+    &--range-start {
+      &:before {
+        right: 0;
+        width: 50%;
+      }
+    }
+
+    &--range-end {
+      &:before {
+        left: 0;
+        width: 50%;
+      }
+    }
+  }
+
+  &__date {
+    position: relative;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 24px;
+    height: 24px;
+    border: 2px solid rgba(0, 0, 0, 0);
+    border-radius: 50%;
+
+    &--today {
+      border-color: #7a9aeb;
+      color: #3468eb;
+    }
+
+    &--selected {
+      background-color: #3468eb;
+      color: #fff;
+    }
+  }
+}
+</style>
---
+++ src/stories/CalendarDateRangeTable.stories.ts
@@ -0,0 +1,52 @@
+import { storiesOf } from "@storybook/vue";
+import { date } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import { Calendar, DateRange } from "@/components/calendar/Calendar";
+import CalendarDateRangeTable from "@/components/calendar/CalendarDateRangeTable.vue";
+
+const selectedDates: DateRange = {
+  min: new Date("2020-03-15"),
+  max: new Date("2020-03-25")
+};
+
+storiesOf("CalendarDateRangeTable", module).add(
+  "default",
+  () => ({
+    components: { CalendarDateRangeTable },
+    data() {
+      return {
+        selectedDates
+      };
+    },
+    props: {
+      date: {
+        default: myDateKnob("Calendar", new Date())
+      }
+    },
+    methods: {
+      onClick: action("click")
+    },
+    computed: {
+      calendar() {
+        const props: any = this.$props;
+        const date: Date = props.date;
+        return Calendar.build(date);
+      }
+    },
+    template: `
+      <CalendarDateRangeTable
+       :calendar="calendar"
+       :selected-dates="selectedDates"
+       @click="onClick" />`
+  }),
+  {
+    info: {
+      summary: "Simple CalendarDateRangeTable component"
+    }
+  }
+);
+
+function myDateKnob(name: string, value: Date) {
+  const stringTimestamp = date(name, value);
+  return new Date(stringTimestamp);
+}
