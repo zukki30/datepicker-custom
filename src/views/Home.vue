321c56f13c3f5329a9922e52acc7906b0b430d43
kk-watanabe <kazuki.watanabe@bebit.com>
2020-03-04 17:25:49 +0900

    feat: Add components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,56 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <h2>DatePicker - 1</h2>
+    <DateRangePicker
+      v-model="value"
+      :shortcuts="shortcuts"
+      :disabled-date="disabledDate"
+    />
+
+    <h2>DatePicker - 2</h2>
+    <PeriodSpecification />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Model, Prop } from "vue-property-decorator";
+import {
+  disabledDateForDatePicker,
+  dateRangePickerShortcuts
+} from "@/util/date-range-picker-util";
+import DateRangePicker from "@/components/DateRangePicker.vue";
+import PeriodSpecification from "@/components/PeriodSpecification.vue";
 
-@Component
-export default class Home extends Vue {}
+@Component({
+  components: { DateRangePicker, PeriodSpecification }
+})
+export default class Home extends Vue {
+  value = null;
+
+  get disabledDate() {
+    return disabledDateForDatePicker();
+  }
+
+  get shortcuts() {
+    return dateRangePickerShortcuts(new Date());
+  }
+}
 </script>
+
+<style scoped lang="scss">
+.home {
+  margin-right: auto;
+  margin-left: auto;
+  padding-top: 100px;
+  max-width: 1000px;
+
+  h2 {
+    margin-bottom: 30px;
+    font-weight: bold;
+    font-size: 36px;
+    &:not(:first-child) {
+      margin-top: 40px;
+    }
+  }
+}
+</style>
