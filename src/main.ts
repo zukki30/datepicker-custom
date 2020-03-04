fbfbeda7c1fec7ed09601adeddf76bd9f5176526
kk-watanabe <kazuki.watanabe@bebit.com>
2020-03-04 18:21:31 +0900

    fix: Re build docs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 
@@ -18,6 +17,5 @@ require("@/assets/scss/index.scss");
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount("#app");
