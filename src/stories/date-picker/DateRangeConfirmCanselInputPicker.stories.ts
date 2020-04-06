7535cc0f5fd586b3e207c9def9fb7e0488a69476
kk-watanabe <kazuki.watanabe@bebit.com>
2020-04-06 12:00:54 +0900

    feat: Add story and component to DateRangeConfirmCanselInputPicker

---
+++ src/stories/date-picker/DateRangeConfirmCanselInputPicker.stories.ts
@@ -0,0 +1,93 @@
+import { storiesOf } from "@storybook/vue";
+import { boolean, text, select } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import { DateRange } from "@/components/calendar/Calendar";
+import { PopupAlign } from "@/components/date-picker/DatePicker";
+import DateRangeConfirmCanselInputPicker from "@/components/date-picker/DateRangeConfirmCanselInputPicker.vue";
+
+const selectedDates: DateRange = {
+  min: new Date("2020-02-15"),
+  max: new Date("2020-03-03")
+};
+
+const disabledDates: DateRange = {
+  min: new Date("2020-01-15"),
+  max: new Date("2020-03-20")
+};
+
+storiesOf("date-picker/DateRangeConfirmCanselInputPicker", module)
+  .add(
+    "default",
+    () => ({
+      components: { DateRangeConfirmCanselInputPicker },
+      data() {
+        return {
+          disabledDates,
+          selectedDates
+        };
+      },
+      props: {
+        disabled: {
+          default: boolean("Disabled", false)
+        },
+        width: {
+          default: text("Width", "350px")
+        },
+        align: {
+          default: select("Align", PopupAlign, PopupAlign.Center)
+        }
+      },
+      methods: {
+        onInput: action("input")
+      },
+      template: `
+      <DateRangeConfirmCanselInputPicker
+       :width="width"
+       :disabled="disabled"
+       :align="align"
+       :disabled-dates="disabledDates"
+       @input="onInput"
+      />`
+    }),
+    {
+      info: {
+        summary: "Simple DateRangeConfirmCanselInputPicker component"
+      }
+    }
+  )
+  .add(
+    "set date",
+    () => ({
+      components: { DateRangeConfirmCanselInputPicker },
+      data() {
+        return {
+          disabledDates,
+          selectedDates
+        };
+      },
+      props: {
+        disabled: {
+          default: boolean("Disabled", false)
+        },
+        width: {
+          default: text("Width", "350px")
+        }
+      },
+      methods: {
+        onInput: action("input")
+      },
+      template: `
+      <DateRangeConfirmCanselInputPicker
+       :width="width"
+       :disabled="disabled"
+       :disabled-dates="disabledDates"
+       :selected-dates="selectedDates"
+       @input="onInput"
+      />`
+    }),
+    {
+      info: {
+        summary: "Simple DateRangeConfirmPicker component"
+      }
+    }
+  );
