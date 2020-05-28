2cc0f07f1e0dba477a21068f4527e2683ceccd3a
kk-watanabe <kazuki.watanabe@bebit.com>
2020-05-28 21:06:33 +0900

    feat: Add DatePicker

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,61 +1,49 @@
 <template>
   <div class="home">
-    test
+    <div class="list">
+      <h2>POPUP 上はみ出し有効</h2>
+      <DateRangePicker v-model="value1" />
+    </div>
+
+    <div class="list">
+      <h2>POPUP 上はみ出し無効</h2>
+      <DateRangePicker v-model="value2" :is-fixed-popup-top-position="false" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Model, Prop } from "vue-property-decorator";
+import { DateRange } from "@/components/date-picker/DateRange.ts";
+import DateRangePicker from "@/components/date-picker/DateRangePicker.vue";
 
-@Component
-export default class Home extends Vue {}
+@Component({
+  components: { DateRangePicker }
+})
+export default class Home extends Vue {
+  value1: DateRange | null = null;
+  value2: DateRange | null = null;
+}
 </script>
 
 <style scoped lang="scss">
 .home {
   margin-right: auto;
   margin-left: auto;
-  padding: 100px 100px 500px;
+  padding: 300px 100px;
+  min-height: 1300px;
   max-width: 1000px;
+
+  .list {
+    &:not(:first-child) {
+      margin-top: 150px;
+    }
+  }
 
   h2 {
     margin-bottom: 20px;
     font-weight: bold;
-    font-size: 36px;
-    &:not(:first-child) {
-      margin-top: 60px;
-    }
+    font-size: 20px;
   }
-
-  p {
-    margin-bottom: 20px;
-    padding-left: 10px;
-    line-height: 1.5;
-  }
-}
-
-.header {
-  padding: 10px;
-  border-bottom: 1px solid $colorBase400;
-}
-
-.datepicker2 {
-  position: relative;
-  z-index: 100;
-}
-
-.datepicker3A {
-  position: relative;
-  z-index: 50;
-}
-
-.datepicker3B {
-  position: relative;
-  z-index: 40;
-}
-
-.datepicker4A {
-  position: relative;
-  z-index: 35;
 }
 </style>
